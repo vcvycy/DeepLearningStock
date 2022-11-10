107e8658ea46aad1c2ea2cec0368b313feda00c7
chenjianfeng.xmu <chenjianfeng.xmu@bytedance.com>
2022-11-10 18:03:18 +0800

    支持fid occurence过滤

--- src/model_train.py
+++ src/model_train.py
@@ -5,6 +5,7 @@ import logging
 import yaml
 import random
 import tensorflow as tf
+import logging
 def load_instance(files):
     if not isinstance(files, list):
         files = [files]
@@ -15,11 +16,9 @@ def load_instance(files):
             size, data = read_file_with_size(f, Instance)
             if size == 0:
                 break
-            instances.append(data)
-    print('-'*100)
-    print("总训练样本: %s" %(len(instances)))
-    print("最后一个样本: %s" %(instances[-1]))
-    print('-'*100)
+            instances.append(data) 
+    logging.info("总训练样本: %s" %(len(instances)))
+    logging.info("最后一个样本: %s" %(instances[-1])) 
     return instances
 
 class TrainItem():
@@ -33,6 +32,8 @@ class TrainData():
     def __init__(self, instances, conf):
         self.instances = instances
         self.conf = conf 
+        # 初始化每个fid的出现次数
+        self.__init_fid2occu()
         # 给每个fid一个index，用于找embedding
         self.__init_fid2index()
         # 初始化训练数据
@@ -76,21 +77,37 @@ class TrainData():
         logging.error("文件中的instance数: %s, 异常数: %s, 可训练数量: %s" %(len(self.instances), invalid_num, len(self.train_items)))
         return 
 
-    def __init_fid2index(self):
-        self.fid2index = {}  # 每个fid对应一个Index，通过Index找embedding 
-        self.index2fid = {}
-        index = 0
+    def __init_fid2occu(self):
+        self.fid2occur = {}   # fid出现次数
         for ins in self.instances: 
             for fc in ins.feature:
                 for fid in fc.fids:
-                    if fid not in self.fid2index:
-                        self.fid2index[fid] = index
-                        self.index2fid[index] = fid
-                        index += 1 
-        
-        print("-" * 100)
-        print("不同总fid数: %s" %(index)) 
-        print("-" * 100)
+                    self.fid2occur[fid] = self.fid2occur.get(fid, 0) + 1
+        return 
+
+    def __init_fid2index(self):
+        self.fid2index = {}  # 每个fid对应一个Index，通过Index找embedding 
+        self.index2fid = {}
+        self.fid2feature = {}
+        index = 0
+        min_fid_occur = self.conf.get("min_fid_occurrence", 0)
+        fid_filtered_num = 0
+        for ins in self.instances: 
+            for fc in ins.feature:
+                for i in range(len(fc.fids)):
+                    fid = fc.fids[i]
+                    if self.fid2occur[fid] < min_fid_occur:
+                        fid_filtered_num +=1
+                        continue
+                    else:
+                        raw_feature = fc.raw_feature[i]
+                        if fid not in self.fid2index:
+                            self.fid2index[fid] = index
+                            self.index2fid[index] = fid
+                            self.fid2feature[fid] = raw_feature 
+                            index += 1  
+        logging.info("过滤后fid数: %s; fid出现次数<%s次，过滤数量: %s" %(index, min_fid_occur, fid_filtered_num))  
+        exit(0)
         return 
     
     def get_fid_num(self):
@@ -115,9 +132,7 @@ class Model:
         bias_num = self.fid_num
         weight_initer = tf.truncated_normal_initializer(mean=0.0, stddev=0.01)
         self.sparse_bias = tf.get_variable(name="bias", dtype=tf.float32, shape=[bias_num], initializer=weight_initer)
-        print("-"*100)
-        print("bias num: %s %s" %(bias_num, self.sparse_bias))
-        print("-"*100)
+        logging.info("bias num: %s %s" %(bias_num, self.sparse_bias)) 
         return 
     
     def get_pred_and_loss(self, nn_out, label):
@@ -140,12 +155,12 @@ class Model:
         """
         # 输入placehold
         self.bias_fid_gate = tf.placeholder(tf.float32, [None, self.fid_num], name="bias_fid_gate")  # 长度为所有fid数量， 1表示有这个Fid， 0表示没有这个fid
-        print("dense model: %s %s" %(self.sparse_bias, self.bias_fid_gate))
+        logging.info("dense model: %s %s" %(self.sparse_bias, self.bias_fid_gate))
         bias_input = self.bias_fid_gate * self.sparse_bias                                 # 乘以开关
         
         # 过Dense nn, 得到pred和loss
         nn_out = tf.reduce_sum(bias_input, axis = 1)
-        print("bias_input:%s nn_out: %s" %(bias_input, nn_out))
+        logging.info("bias_input:%s nn_out: %s" %(bias_input, nn_out))
 
         self.label = tf.placeholder(tf.float32, [None], name = "label")
         self.pred, self.loss = self.get_pred_and_loss(nn_out, self.label)
@@ -203,29 +218,35 @@ class Model:
             # 开始训练
             _, pred, loss=sess.run([self.optimizer, self.pred, self.loss],
                     feed_dict = feed_dict)
-            print("pred: %s; loss: %s" %(pred, loss))
-            print("[train-epoch:%s]" %(i+1))
+            if i % 10 == 0:
+                logging.info("[train-epoch:%s] loss: %s, pred:%s" %(i+1, loss, pred[:5]))
             #
         
         # 结果:
-        bias_value = sess.run(self.sparse_bias)
-        print("bias_value: %s" %(bias_value))
-        for i in range(len(train_data.index2fid)):
-            fid = train_data.index2fid[i]
-            print("Fid: %s bias: %.3f" %(fid, bias_value))
+        bias_value = sess.run(tf.sigmoid(self.sparse_bias))
+        logging.info("bias_value: %s" %(bias_value))
+        features = []
+        for fid in train_data.fid2index:
+            feature = train_data.fid2feature[fid]
+            features.append((fid>>54, fid, train_data.fid2index[fid], feature, train_data.fid2occur[fid])) 
+
+        features.sort(key= lambda x : "%s-%s" %(x[0], x[3])) # 按slot排序
+        for slot, fid, index, feature, occur in features: 
+            logging.info("[slot: %s %s] [次数: %5s] [bias: %.3f]  (%s)" %(slot, fid, occur, bias_value[index], feature))
         return 
 
 if __name__ == "__main__":
     ## 载入yaml配置文件
     conf = yaml.safe_load(open("model_train.yaml", 'r') .read())
     files = conf.get("train_files") 
+    logging.basicConfig(filename=conf.get("log_file"), format = '%(levelname)s %(asctime)s %(message)s', level=logging.DEBUG)
     # 载入instance
+    logging.info("START")
+    logging.info("conf: %s" %(conf)) 
     instances = load_instance(files)
-    # 处理instance
-    train_data = TrainData(instances, conf.get("model_data")) 
-    print(train_data.get_mini_batch())
+    # 预处理instance
+    train_data = TrainData(instances, conf.get("model_data"))  
 
     # 所有Fid的数量，用于模型初始化embedding/bias
-    all_fid_num = train_data.get_fid_num()
-    model = Model(conf.get("model"), all_fid_num)
+    model = Model(conf.get("model"), train_data.get_fid_num())
     model.train(train_data)
